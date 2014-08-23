be4a2a0417bc60679569b8f3faec5256a2c9ce00
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-08-23 14:03:25 +0630

    add in_groups_of to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@users = User.all.in_groups_of(4) 
+		@users = User.all.in_groups_of(4,false) 
 	end
 
 	def new
