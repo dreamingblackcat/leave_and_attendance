209832ca512b8f8562bdb4151ea0b76b0779dadb
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-08-23 11:03:41 +0630

    added team scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823043121) do
+ActiveRecord::Schema.define(version: 20140823043321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
