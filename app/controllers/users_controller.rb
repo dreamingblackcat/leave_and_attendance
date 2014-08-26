fae0d2ec8b515207a8018afaf357685e9fe1c883
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-08-26 15:54:15 +0630

    fixed password form showing in user edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,6 @@ class UsersController < ApplicationController
 
 	def new
 		@user = User.new
-
-		respond_to do |format|
-		  if @user.save
-		    format.html { redirect_to @user, notice: 'User was successfully created.' }
-		    format.json { render :show, status: :created, location: @user }
-		  else
-		    format.html { render :new }
-		    format.json { render json: @user.errors, status: :unprocessable_entity }
-		  end
-		end
 		
 	end
 
