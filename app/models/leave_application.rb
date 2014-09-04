ff77dbbc71d345a75e5fb06a9a1f9d2bfb526edb
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-09-04 17:23:33 +0630

    adding user select field and corresponding index and show page

--- app/models/leave_application.rb
+++ app/models/leave_application.rb
@@ -1,5 +1,5 @@
 class LeaveApplication < ActiveRecord::Base
   belongs_to :user
-  has_many :leave_dates
+  has_many :leave_dates, dependent: :destroy
   accepts_nested_attributes_for :leave_dates, allow_destroy: true
 end
--- app/models/leave_period.rb
+++ app/models/leave_period.rb
@@ -1,2 +1,3 @@
 class LeavePeriod < ActiveRecord::Base
+	has_many :leave_dates
 end
